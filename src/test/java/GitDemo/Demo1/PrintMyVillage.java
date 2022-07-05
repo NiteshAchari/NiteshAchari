129e12c1e8ca8df30c40f627cd99f96b2e33e435
Nitesh <nitesh.achari@igsindia.net>
2022-07-05 10:11:21 +0530

    a

---
+++ src/test/java/GitDemo/Demo1/PrintMyVillage.java
@@ -0,0 +1,10 @@
+package GitDemo.Demo1;
+
+public class PrintMyVillage {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     System.out.println("Dhareshwar");
+	}
+
+}
